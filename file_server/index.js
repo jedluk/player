0c7f0d31743811f8745f6d183fc685cc335c1b28
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2020-08-04 23:51:24 +0200

    dockerize backend

--- file_server/index.js
+++ file_server/index.js
@@ -4,10 +4,11 @@ const cors = require('cors')
 const path = require('path')
 
 const {
-  PORT = 8083,
+  PORT = '8083',
   UPLOAD_DIR = path.join(__dirname, 'uploads'),
   CLIENT_DIR = path.join(__dirname, '..', 'client', 'build'),
 } = process.env
+console.log({ PORT, UPLOAD_DIR, CLIENT_DIR })
 
 const CORS = cors({
   origin: 'http://localhost:3000',
@@ -25,4 +26,4 @@ app.use('/uploads', express.static(UPLOAD_DIR))
 app.use(express.static(CLIENT_DIR))
 app.all('*', (_, res) => res.status(400).send('Not found!'))
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(parseInt(PORT, 10), () => console.log(`Listening on port ${PORT}`))
