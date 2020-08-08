c023c61913fce7f554c04be59b038fa2df4baa4a
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2020-08-09 00:55:54 +0200

    css animations

--- client/src/main/Tracks.tsx
+++ client/src/main/Tracks.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import FontAwesome from 'react-fontawesome'
 import { formatTime } from '../utils/lib'
 import AddNewButton from '../common/Button'
@@ -25,20 +25,33 @@ const withoutExtenstion = (track: string) =>
   track.slice(0, track.lastIndexOf('.'))
 
 function MyTracks(props: MyTracksProps) {
-  const gridRef = useRef<HTMLDivElement>(null)
+  const theadRowRef = useRef<HTMLTableRowElement>(null)
+  const [loaded, setLoaded] = useState<boolean>(false)
   const [gridTouched, setGridTouched] = useState<boolean>(false)
+
+  useEffect(() => {
+    setLoaded(false)
+    setTimeout(
+      () => setLoaded(true),
+      (0.5 + ((props.tracks.length * 0.5) % 6)) * 1000
+    )
+  }, [props.tracks.length])
 
   const handleScroll = useCallback(
     e => {
       if ((e.target as HTMLDivElement).scrollTop > 0 && !gridTouched) {
         setGridTouched(true)
-        if (gridRef.current !== null) {
-          gridRef.current.classList.toggle(style.active)
+        if (theadRowRef.current !== null) {
+          theadRowRef.current.childNodes.forEach((node: any) =>
+            node.classList.toggle(style.active)
+          )
         }
       } else if ((e.target as HTMLDivElement).scrollTop === 0) {
         setGridTouched(false)
-        if (gridRef.current !== null) {
-          gridRef.current.classList.toggle(style.active)
+        if (theadRowRef.current !== null) {
+          theadRowRef.current.childNodes.forEach((node: any) =>
+            node.classList.toggle(style.active)
+          )
         }
       }
     },
@@ -53,49 +66,68 @@ function MyTracks(props: MyTracksProps) {
         </h1>
         <AddNewButton action={props.onAdd} />
       </div>
-      <div
-        className={style['tracks-grid']}
-        ref={gridRef}
-        onScroll={handleScroll}
-      >
-        <div>Title</div>
-        <div>Artist</div>
-        <div>Album</div>
-        <div>Year</div>
-        <div>Genre</div>
-        <div>
-          <FontAwesome name="calendar" style={{ fontSize: '0.8rem' }} />
-        </div>
-        <div></div>
-        {props.tracks.map(track => (
-          <React.Fragment key={track.title}>
-            <div
-              className={style['action-play']}
-              style={{
-                fontWeight:
-                  props.currentTrack === track.url ? 'bold' : 'normal',
-              }}
-              onClick={() => props.setTrack(track.url)}
-            >
-              {track.title.length > 70
-                ? withoutExtenstion(track.title).slice(0, 70) + ' (...) '
-                : track.title}
-              <span>
-                <FontAwesome name="volume-up" />
-              </span>
-            </div>
-            <div>{track.artist}</div>
-            <div>{track.album}</div>
-            <div>{track.year}</div>
-            <div>{track.genre}</div>
-            <div>{formatTime(track.uploaded)}</div>
-            <div>
-              <a download={track.title} href={track.url}>
-                <FontAwesome name="arrow-circle-o-down" style={downloadStyle} />
-              </a>
-            </div>
-          </React.Fragment>
-        ))}
+      <div className={style['grid-container']} onScroll={handleScroll}>
+        <table className={style['tracks-grid']}>
+          {props.tracks.length ? (
+            <React.Fragment>
+              <thead>
+                <tr ref={theadRowRef}>
+                  <th>Title</th>
+                  <th>Artist</th>
+                  <th>Album</th>
+                  <th>Year</th>
+                  <th>Genre</th>
+                  <th>
+                    <FontAwesome
+                      name="calendar"
+                      style={{ fontSize: '0.8rem' }}
+                    />
+                  </th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {props.tracks.map((track, idx) => (
+                  <tr
+                    key={track.title}
+                    className={!loaded ? style.animate : undefined}
+                    style={{
+                      animationDuration: `${0.2 + ((idx * 0.5) % 6)}s`,
+                    }}
+                  >
+                    <td
+                      className={style['action-play']}
+                      style={{
+                        fontWeight:
+                          props.currentTrack === track.url ? 'bold' : 'normal',
+                      }}
+                      onClick={() => props.setTrack(track.url)}
+                    >
+                      {track.title.length > 70
+                        ? withoutExtenstion(track.title).slice(0, 70) +
+                          ' (...) '
+                        : track.title}
+                      <FontAwesome name="volume-up" />
+                    </td>
+                    <td>{track.artist}</td>
+                    <td>{track.album.slice(0, 10)}</td>
+                    <td>{track.year}</td>
+                    <td>{track.genre}</td>
+                    <td>{formatTime(track.uploaded)}</td>
+                    <td>
+                      <a download={track.title} href={track.url}>
+                        <FontAwesome
+                          name="arrow-circle-o-down"
+                          style={downloadStyle}
+                        />
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </React.Fragment>
+          ) : null}
+        </table>
       </div>
     </div>
   )
