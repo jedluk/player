ce6addb116e3b757682c8a88ac4d68ca205f8cff
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2020-09-13 20:24:37 +0200

    change binary gradient to fuzzy

--- client/src/main/VolumeSetter.tsx
+++ client/src/main/VolumeSetter.tsx
@@ -1,54 +1,53 @@
-import React, { RefObject, useCallback, useState } from 'react'
-import FontAwesome from 'react-fontawesome'
-import { joinClasses } from '../utils/lib'
+import React, { RefObject, useCallback, useState } from "react";
+import FontAwesome from "react-fontawesome";
+import { joinClasses } from "../utils/lib";
 
-import style from './VolumeSetter.module.css'
+import style from "./VolumeSetter.module.css";
 
 interface VolumeSetterProps {
-  audio: RefObject<HTMLAudioElement>
+  audio: RefObject<HTMLAudioElement>;
 }
 
 export default function VolumeSetter({ audio: audioRef }: VolumeSetterProps) {
-  const [volume, setVolume] = useState<number>(0.8)
+  const [volume, setVolume] = useState<number>(0.5);
 
-  const audio = audioRef !== null ? audioRef.current : null
+  const audio = audioRef !== null ? audioRef.current : null;
 
   const changeVolume = useCallback(
-    step => {
+    (step) => {
       if (audio !== null) {
-        const nextValue = +(volume + step).toFixed(2)
+        const nextValue = +(volume + step).toFixed(2);
         if (nextValue >= 0 && nextValue <= 1) {
-          audio.volume = nextValue
-          setVolume(nextValue)
+          audio.volume = nextValue;
+          setVolume(nextValue);
         }
       }
     },
-    [setVolume, volume, audio]
-  )
+    [setVolume, volume, audio],
+  );
 
   return (
     <button
       className={style.root}
       style={{
-        color: 'black',
-        background: `linear-gradient(90deg, #545454 ${
-          volume * 100
-        }%, #d3d3d3 0%)`,
+        color: "black",
+        background: `linear-gradient(to top, #545454 ${volume *
+          100}%, #d3d3d3)`,
       }}
     >
       <FontAwesome name="volume-off" />
       <div
-        className={joinClasses(style.setter, style['setter-up'])}
-        onClick={() => changeVolume(0.02)}
+        className={joinClasses(style.setter, style["setter-up"])}
+        onClick={() => changeVolume(0.05)}
       >
         <FontAwesome name="volume-up" style={{ fontSize: 10 }} />
       </div>
       <div
-        className={joinClasses(style.setter, style['setter-down'])}
-        onClick={() => changeVolume(-0.02)}
+        className={joinClasses(style.setter, style["setter-down"])}
+        onClick={() => changeVolume(-0.05)}
       >
         <FontAwesome name="volume-down" style={{ fontSize: 10 }} />
       </div>
     </button>
-  )
+  );
 }
