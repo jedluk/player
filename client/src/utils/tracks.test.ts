ed21b3cb99bb34d569a354a58bf7b85ba35cdb9c
Jedrzej Lukasiuk <jedrzej.lukasiuk@here.com>
2020-11-15 16:31:27 +0100

    remove outdated tests

--- client/src/utils/tracks.test.ts
+++ client/src/utils/tracks.test.ts
@@ -2,15 +2,9 @@ import {
   serializeTracks,
   matchTitle,
   findNextTrack,
-  hasParent,
-  previousDir,
   generateModifiers,
   filterTracks,
 } from './tracks'
-
-jest.mock('./config', () => ({
-  FILE_SEPARATOR: '/',
-}))
 
 describe('track utils test suite', () => {
   describe('serializeTracks', () => {
@@ -33,26 +27,6 @@ describe('track utils test suite', () => {
 
     it('returns predicate which passess matched songs otherwise', () => {
       expect(tracks.filter(matchTitle('easy'))).toEqual(tracks.slice(0, 1))
-    })
-  })
-
-  describe('hasParent function', () => {
-    it('returns false if there is no parent in URI path', () => {
-      expect(hasParent({ url: '/someDir' } as any)).toEqual(false)
-    })
-
-    it('returns true if there is a parent in URI path', () => {
-      expect(hasParent({ url: 'root/someDir' } as any)).toEqual(false)
-    })
-  })
-
-  describe('previousDir function', () => {
-    it('returns undefined if item has no parent', () => {
-      expect(previousDir({ url: 'dir/item' } as any)).toEqual(undefined)
-    })
-
-    it('returns parent URI otherwise', () => {
-      expect(previousDir({ url: 'root/dir/item' } as any)).toEqual('root')
     })
   })
 
