52146af74c3a4aa3870a8553b654ad379495b060
Jedrzej Lukasiuk <jedrzej.lukasiuk@here.com>
2020-11-15 17:40:05 +0100

    fixed issue with path validation and wrong import in getFiles handler

--- server/src/handlers/getFiles.js
+++ server/src/handlers/getFiles.js
@@ -1,20 +1,22 @@
-const fs = require('fs').promises
+const fs = require('fs')
 
 function getPartials(range, total) {
   const [partialStart, partialEnd] = range.replace(/bytes=/, '').split('-')
   const start = parseInt(partialStart, 10) || 0
   const end = partialEnd ? parseInt(partialEnd, 10) : total - 1
+  console.log({ start, end })
   return { start, end }
 }
 
 async function streamFile(req, res) {
-  const stat = await fs.stat(file)
+  const { path } = req.query
+  const stat = await fs.promises.stat(path)
   const { size: total } = stat
   const { range } = req.headers
 
   const { start, end } = getPartials(range, total)
   const chunkSize = end - start + 1
-  const rstream = fs.createReadStream(file, { start, end })
+  const rstream = fs.createReadStream(path, { start, end })
 
   res.writeHead(206, {
     'Content-Range': ['bytes ', start, '-', end, '/', total].join(''),
--- server/src/middleware/validatreFileQuery.js
+++ server/src/middleware/validatreFileQuery.js
@@ -10,7 +10,7 @@ module.exports = async function checkQuery(req, res, next) {
     return res.status(400).send({ msg: '"path" query param must be defined' })
   }
 
-  if (!isUndefined(path) && SUPPORTED_FILES.some(type => path.endsWith(type))) {
+  if (!isUndefined(path) && !SUPPORTED_FILES.some(type => path.endsWith(type))) {
     return res
       .status(400)
       .send({ msg: `Only ${String(SUPPORTED_FILES)} are suppoerted` })
@@ -20,6 +20,7 @@ module.exports = async function checkQuery(req, res, next) {
     await fs.access(path)
     return next()
   } catch (err) {
+    console.log({ err })
     return res.status(400).send({ msg: '"path" is not pointing to valid file' })
   }
 }
