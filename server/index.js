45fc1ba6910b225cc1ea0b20d343c5c785c59f47
Jedrzej Lukasiuk <jedrzej.lukasiuk@here.com>
2020-11-15 10:49:59 +0100

    extract error handler middleware, clean up index file

--- server/index.js
+++ server/index.js
@@ -1,15 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const path = require('path')
-const serveStatic = require('serve-static')
 
-const {
-  SERVER_MODE,
-  NODE_ENV,
-  PORT = '8083',
-  ASSETS_DIR = path.join(__dirname, '..', 'assets'),
-} = process.env
+const { NODE_ENV, PORT = '8083' } = process.env
 
 function makeServer(port = parseInt(PORT, 10)) {
   const app = express()
@@ -20,24 +13,13 @@ function makeServer(port = parseInt(PORT, 10)) {
     })
   )
   app.use('/api', require('./routes'))
-  app.use(
-    '/static',
-    serveStatic(path.join(ASSETS_DIR), {
-      setHeaders: function (res, path) {
-        res.setHeader('Content-Type', 'audio/mpeg')
-      },
-    })
-  )
   app.all('*', (_, res) => res.status(400).send({ msg: 'Not found!' }))
-  app.use(function (err, req, res, next) {
-    res
-      .status(err.status || 500)
-      .send({ msg: err.message || 'Internal server error' })
-  })
+  app.use(require('./middleware/errorHandler'))
   app.listen(port, () => console.log(`Listening on port ${PORT}`))
 }
 
-if (SERVER_MODE === 'standalone') {
+if (['production', 'development'].includes(NODE_ENV)) {
   makeServer()
 }
+
 module.exports = { run: makeServer }
