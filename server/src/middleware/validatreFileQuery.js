8c859d47f83b719c398af389387714e9fd6a9288
Jedrzej Lukasiuk <jedrzej.lukasiuk@here.com>
2020-11-15 13:59:06 +0100

    extend file validation

--- server/src/middleware/validatreFileQuery.js
+++ server/src/middleware/validatreFileQuery.js
@@ -1,4 +1,7 @@
+const { isUndefined } = require('../lib/utils')
 const fs = require('fs').promises
+
+const SUPPORTED_FILES = ['mp3']
 
 module.exports = async function checkQuery(req, res, next) {
   const { path } = req.query
@@ -7,12 +10,18 @@ module.exports = async function checkQuery(req, res, next) {
     return res.status(400).send({ msg: '"path" query param must be defined' })
   }
 
+  if (!isUndefined(path) && SUPPORTED_FILES.some(type => path.endsWith(type))) {
+    return res
+      .status(400)
+      .send({ msg: `Only ${String(SUPPORTED_FILES)} are suppoerted` })
+  }
+
   await fs.exists(path, err => {
     if (err !== null) {
       return res
         .status(400)
         .send({ msg: '"path" is not pointing to valid file' })
     }
-    next()
+    return next()
   })
 }
