cd0df43e77f00fd5b7d36105feba121059762dc1
Jedrzej Lukasiuk <jedrzej.lukasiuk@here.com>
2020-11-22 19:32:49 +0100

    fixed spelling issue

--- client/src/view/scheme/SomeAvailable.tsx
+++ client/src/view/scheme/SomeAvailable.tsx
@@ -6,7 +6,7 @@ export default function SomeAvailable(): JSX.Element {
   return (
     <div className={style.container}>
       <h1>Current folder does not contain mp3 file(s)</h1>
-      <h3>Please try to choose one of existing subfolder</h3>
+      <h3>Please try to choose one of existing subfolder(s)</h3>
     </div>
   )
 }
