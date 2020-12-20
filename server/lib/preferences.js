dceb3dd34b03a68a2a49c4c4556729cbd7159e7e
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2020-12-20 18:01:10 +0100

    add preferences object

---
+++ server/lib/preferences.js
@@ -0,0 +1,49 @@
+const { promises } = require('fs')
+const path = require('path')
+
+class Preferences {
+  constructor() {
+    this.preferencesPath = path.join(__dirname, '..', 'preferences.json')
+    this.dataModel = {
+      directory: 'string',
+      theme: Array(6)
+        .fill(1)
+        .map((_, i) => `theme${i + 1}`),
+      language: ['pl', 'en'],
+    }
+  }
+
+  isValid(newPref) {
+    return (
+      typeof newPref === 'object' &&
+      Object.keys(newPref).length === Object.keys(this.dataModel).length &&
+      Object.entries(newPref).every(([key, value]) =>
+        key in this.dataModel && Array.isArray(this.dataModel[key])
+          ? this.dataModel[key].includes(value)
+          : typeof value === this.dataModel[key]
+      )
+    )
+  }
+
+  read() {
+    return new Promise((resolve, reject) => {
+      promises
+        .readFile(this.preferencesPath)
+        .then(JSON.parse)
+        .then(file => (this.isValid(file) ? resolve(file) : reject(null)))
+        .catch(() => reject(null))
+    })
+  }
+
+  write(preferences) {
+    return new Promise((resolve, reject) => {
+      if (!this.isValid(preferences)) reject({ msg: 'Not a valid preferences' })
+      promises
+        .writeFile(this.preferencesPath, JSON.stringify(preferences))
+        .then(() => resolve({ msg: 'Saved' }))
+        .catch(() => reject({ msg: 'Could not save a file' }))
+    })
+  }
+}
+
+module.exports = Preferences
---
+++ server/lib/preferences.test.js
@@ -0,0 +1,111 @@
+const Preferences = require('./preferences')
+const { promises } = require('fs')
+
+jest.mock('fs/promises')
+
+describe('Preferences', () => {
+  let preferences
+
+  beforeEach(() => {
+    preferences = new Preferences()
+  })
+
+  describe('isValid method', () => {
+    it('returns false if arg is primitive', () => {
+      expect(preferences.isValid('something')).toEqual(false)
+      expect(preferences.isValid(2)).toEqual(false)
+      expect(preferences.isValid(true)).toEqual(false)
+    })
+
+    it('returns false if object does not contain all required properties', () => {
+      const pref = {
+        language: 'pl',
+      }
+      expect(preferences.isValid(pref)).toEqual(false)
+    })
+
+    it('returns true if given object match model', () => {
+      const pref = {
+        language: 'pl',
+        theme: 'theme1',
+        directory: '/Users/jedrzej/Desktop/muse',
+      }
+      expect(preferences.isValid(pref)).toEqual(true)
+    })
+  })
+
+  describe('read method', () => {
+    let preferencesObject
+
+    beforeEach(() => {
+      preferencesObject = {
+        language: 'pl',
+        theme: 'theme1',
+        directory: '/Users/jedrzej/Desktop/muse',
+      }
+    })
+
+    it('rejects a promise with null when file does not exist', async () => {
+      promises.readFile = jest.fn().mockRejectedValueOnce('Not exists!')
+
+      expect.assertions(1)
+      await preferences.read().catch(err => expect(err).toEqual(null))
+    })
+
+    it('rejects a promise with null when file is not valid preferences object', async () => {
+      delete preferencesObject.theme
+      promises.readFile = jest
+        .fn()
+        .mockResolvedValueOnce(JSON.stringify(preferencesObject))
+
+      expect.assertions(1)
+      await preferences.read().catch(err => expect(err).toEqual(null))
+    })
+
+    it('resolves promise with preferences object if valid preference object exists', async () => {
+      promises.readFile = jest
+        .fn()
+        .mockResolvedValueOnce(JSON.stringify(preferencesObject))
+
+      const res = await preferences.read()
+      expect(res).toEqual(preferencesObject)
+    })
+  })
+
+  describe('write method', () => {
+    let preferencesObject
+
+    beforeEach(() => {
+      preferencesObject = {
+        language: 'pl',
+        theme: 'theme1',
+        directory: '/Users/jedrzej/Desktop/muse',
+      }
+    })
+
+    it('rejects a promise when preferences are not a valid object', async () => {
+      delete preferencesObject.theme
+      promises.writeFile = jest.fn()
+      expect.assertions(1)
+      await preferences
+        .write(preferencesObject)
+        .catch(err => expect(err.msg).toBeDefined())
+    })
+
+      it('rejects a promise when writeFile throws', async () => {
+        promises.writeFile = jest.fn().mockRejectedValueOnce('error')
+
+        expect.assertions(1)
+        await preferences
+          .write(preferencesObject)
+          .catch(err => expect(err.msg).toBeDefined())
+      })
+
+      it('resolves promise when file is saved', async () => {
+        promises.writeFile = jest.fn().mockResolvedValueOnce('ok')
+
+        const res = await preferences.write(preferencesObject)
+        expect(res.msg).toBeDefined()
+      })
+  })
+})
