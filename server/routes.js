4e60966e5275fb7fb51354ac1885f49c53e5695d
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2021-01-17 15:41:57 +0100

    improve semantic

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const validateQuery = require('./validators/validateDirQuery')
+const validateDirQuery = require('./validators/validateDirQuery')
 const validateFileQuery = require('./validators/validateFileQuery')
 
 const router = express.Router()
 
-router.get('/dirs', ...validateQuery, require('./handlers/getDirs'))
+router.get('/dirs', ...validateDirQuery, require('./handlers/getDirs'))
 router.get('/stream/file', ...validateFileQuery, require('./handlers/getFiles'))
 router.get('/preferences', require('./handlers/getPreferences'))
 router.post('/preferences', require('./handlers/postPreferences'))
