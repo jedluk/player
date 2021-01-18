3448a567728496685af9c2eea525470ebee52af7
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2021-01-18 22:16:33 +0100

    improve error handling

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -2,12 +2,12 @@ const ERROR_CODES = require('../errorCodes')
 const { APIError } = require('../lib/error')
 
 module.exports = function (err, req, res, next) {
-  return res.status(err.status || 500).json(
-    err instanceof APIError
-      ? err.serialize()
-      : {
-          message: 'Internal server error',
-          code: ERROR_CODES.internalError,
-        }
-  )
+  if (err instanceof APIError) {
+    err.log()
+    return res.status(err.status).json(err.serialize())
+  }
+  return res.status(500).json({
+    message: 'Internal server error',
+    code: ERROR_CODES.internalError,
+  })
 }
