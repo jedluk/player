c370becfc0283c06e6e38d8cfb43f11036390f8a
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2021-05-09 21:09:24 +0200

    add option to populate translations

--- client/scripts/i18n.js
+++ client/scripts/i18n.js
@@ -1,21 +1,39 @@
 const fs = require('fs').promises
 const path = require('path')
 
-const enTranslationsPath = [__dirname, '..', 'src', 'translations', 'en.json']
-const typesPath = [__dirname, '..', 'src', 'translations', 'types.ts']
+const translationsPath = [__dirname, '..', 'src', 'translations']
 
-fs.readFile(path.join(...enTranslationsPath))
-  .then(buffer => buffer.toString())
-  .then(json => JSON.parse(json))
-  .then(file => {
-    console.log('Generating types...')
-    fs.writeFile(
-      path.join(...typesPath),
-      Buffer.from(
-        [
-          'export type TranslationKey = ',
-          ...Object.keys(file).map(key => `  | '${key}'`),
-        ].join('\n')
+const toTypes = () =>
+  fs
+    .readFile(path.join(...translationsPath, 'en.json'))
+    .then(buffer => buffer.toString())
+    .then(json => JSON.parse(json))
+    .then(file => {
+      console.log('Generating types...')
+      fs.writeFile(
+        path.join(...translationsPath, 'types.ts'),
+        Buffer.from(
+          [
+            'export type TranslationKey = ',
+            ...Object.keys(file).map(key => `  | '${key}'`),
+          ].join('\n')
+        )
       )
-    )
-  })
+    })
+
+const populate = () =>
+  fs
+    .readFile(path.join(...translationsPath, 'en.json'))
+    .then(buffer => JSON.parse(buffer.toString()))
+    .then(template => {
+      const content = Object.fromEntries(
+        Object.entries(template).map(([k, v]) => [k, `lang::[${v}]`])
+      )
+      fs.writeFile(
+        path.join(...translationsPath, 'lang.json'),
+        JSON.stringify(content, undefined, 4)
+      )
+    })
+
+const func = process.argv[2] === 'populate' ? populate : toTypes
+func.call()
